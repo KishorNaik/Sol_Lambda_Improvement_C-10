8cd6ac5ec1a0cadd499aaf1de8ea41caef998b86
Kishor Naik <kishor.naik011.net@gmail.com>
2021-11-20 12:23:42 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//C# 9
+Func<string, int> toInt1 = (string value) => int.Parse(value);
+Console.WriteLine(toInt1("1"));
+
+// C#10
+var toInt2 = (string value) => int.Parse(value);
+Console.WriteLine(toInt2("1"));
+
+// Explicit Return Type
+var toInt3 = int (string value) => int.Parse(value);
+Console.WriteLine(toInt3("1"));
